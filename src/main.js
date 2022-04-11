25980802cc2e9595d813900351e94ce0d41f9d3c
Jackson <deltarax@gmail.com>
2022-04-11 10:32:42 -0700

    Workin Milestone!

--- src/main.js
+++ src/main.js
@@ -16,3 +16,6 @@ let game = new Phaser.Game({
 // set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
+
+// reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
