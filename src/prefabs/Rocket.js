3db9b7c2abd1f09eaddd7ea989a1f0e5634d0720
Jackson <deltarax@gmail.com>
2022-04-11 16:36:53 -0700

    fixed sound bug

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -6,6 +6,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
       // add object to existing scene
       scene.add.existing(this);
       this.isFiring = false;
+      this.sfxRocket = scene.sound.add('sfx_rocket');
       this.moveSpeed = 2;
     }
 
